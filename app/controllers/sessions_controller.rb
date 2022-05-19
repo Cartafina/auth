819cf84e97261ff2fad5aa02c84528cb0da1b998
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-05-19 01:17:25 +0000

    authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.find_by({ "email" => params["email"]})
+        if @user
+            #if @user["password"] == params["password"]
+            if BCrypt::Password.new(@user) == params["password"]
+                flash["notice"] = "Welcome."
+                redirect_to "/companies"
+            else
+                flash["notice"] = "Nope!"
+                redirect_to "/companies"
+            end
+        else
+            flash["notice"] = "Nope!"
+            redirect_to "/sessions/new"
+        end
+    end
+end
